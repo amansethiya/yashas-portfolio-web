d14718119b223dd45c7712240160bcc000714223
Aman sethiya <124484498+amansethiya@users.noreply.github.com>
2026-01-08 15:45:54 +0530

    Fix Vite scripts and remove homepage/gh-pages config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/yashas-portfolio-web/",
-});
+  base:
+    mode === "client"
+      ? "/" // yashaskarne.github.io
+      : "/yashas-portfolio-web/", // amansethiya.github.io
+}));
