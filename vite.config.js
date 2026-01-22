d090719d18173599eddfce991b8269dd1fb3b4b4
Aman sethiya <124484498+amansethiya@users.noreply.github.com>
2026-01-22 23:30:24 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
   plugins: [react],
-
-  // client = yashaskarne.github.io (root domain)
-  // me = amansethiya.github.io/yashas-portfolio-web
-  base: mode === "client" ? "/" : "/yashas-portfolio-web/",
+  base: "/yashas-portfolio-web/",
 }));
